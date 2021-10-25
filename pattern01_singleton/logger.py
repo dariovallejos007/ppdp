8975f8b63cd051b8aeb84847f4387a31bebef05a
dariovallejos007 <dario.vallejos2021@gmail.com>
2021-10-25 11:39:53 +0800

    added documentation

--- pattern01_singleton/logger.py
+++ pattern01_singleton/logger.py
@@ -5,11 +5,14 @@ import datetime
 
 class Loggers():
     """
+    Logger class that creates *.logs file for any python file from which this is called
     """
     def __init__(self) -> None:
         """
+        init variable self.calling_file is automatically created whenever this Class is called
+        self.calling_file   :  full OS path of the python file from which this Class is called
         """
-        self.calling_file      =  inspect.stack()[-1][1] #see this: https://stackoverflow.com/a/6628348
+        self.calling_file  =  inspect.stack()[-1][1] #see this: https://stackoverflow.com/a/6628348
 
     def _create_logger(self):
         """
@@ -18,6 +21,16 @@ class Loggers():
 
     class _Logger(object):
         """
+        inner Class _Logger
+
+        ATTRIBUTES:
+        self.calling_file      :  same as outer Class Logger.calling_file
+        self.calling_from      :  full OS path of the folder containing the python file calling this Class
+        self.calling_filename  :  file name of the python file calling this Class
+        self.logs_file         :  name of the logs file to be created
+
+        METHODS:
+        self._write_log  :  writes on the log file. to be used by the other log methods
         """
         def __init__(self, logger) -> None:
             self.logger            =  logger
